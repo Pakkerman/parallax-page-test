9b4c37d2a4f03fd82984f8f0fe7eeb376ff88bce
Pakkerman <Pakkermandev.gmail.com>
2023-05-15 20:52:16 +0800

    testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { easings } from "@react-spring/web"
 import {
   SiNextdotjs,
   SiPrisma,
@@ -14,159 +15,197 @@ import {
   IParallaxLayer,
 } from "@react-spring/parallax"
 import { NextPage } from "next/types"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const ProjectView = () => {
   return (
-    <div className="flex h-[100svh] w-full flex-col items-center justify-center">
-      <div className="h-[70%] w-[80%] rounded-xl border-2 border-slate-300"></div>
-      <div className="flex w-[80%] justify-center space-x-2 py-2 md:space-x-4">
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiReact size={36} />
-          <p className="text-sm">React</p>
-        </div>
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiNextdotjs size={36} />
-          <p className="text-sm">Next.js</p>
-        </div>
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiPrisma size={36} />
-          <p className="text-sm">Prisma</p>
-        </div>
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiTrpc size={36} />
-          <p className="text-sm">tRPC</p>
-        </div>
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiTailwindcss size={36} />
-          <p className="text-sm">Tailwind</p>
-        </div>
-        <div className="flex h-16 w-16 flex-col items-center justify-center">
-          <SiTypescript size={36} />
-          <p className="text-sm">TypeScript</p>
+    <div className=" fixed bottom-0 flex h-[90svh] w-full flex-col items-center justify-center">
+      <div className="h-full w-[80%] rounded-xl border-2 border-slate-300">
+        <div className=" flex h-full  w-full  flex-col items-center justify-between">
+          <div className=" preview h-[80%] w-full"> placeholder for image</div>
+          <div className="preview  h-[20%] w-full border-t-2  border-slate-300">
+            project infos
+          </div>
         </div>
       </div>
+      <ul className="flex w-[80%] justify-center space-x-2 py-2 md:space-x-4 ">
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiReact size={36} />
+          <p className="text-sm">React</p>
+        </li>
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiNextdotjs size={36} />
+          <p className="text-sm">Next.js</p>
+        </li>
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiPrisma size={36} />
+          <p className="text-sm">Prisma</p>
+        </li>
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiTrpc size={36} />
+          <p className="text-sm">tRPC</p>
+        </li>
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiTailwindcss size={36} />
+          <p className="text-sm">Tailwind</p>
+        </li>
+        <li className="flex h-16 w-16 flex-col items-center justify-center">
+          <SiTypescript size={36} />
+          <p className="text-sm">TypeScript</p>
+        </li>
+      </ul>
     </div>
   )
 }
 
 const Home: NextPage = () => {
+  // const [load, setLoad] = useState(false)
   const parallax = useRef<IParallax>(null!)
   const layer = useRef<IParallaxLayer>(null!)
+  const fade = useSpring({
+    from: {
+      scale: "105%",
+      opacity: 0,
+      y: "-100px",
+    },
+    to: {
+      scale: "100%",
+      opacity: 1,
+      y: "0px",
+    },
+    delay: 500,
+  })
+
+  const [showMenu, setShowMenu] = useState(false)
+  const menuAnimation = useSpring({
+    x: showMenu ? 0 : 100,
+  })
 
   useEffect(() => {
-    parallax.current.scrollTo(2)
+    // parallax.current.scrollTo(2)
   }, [])
 
   return (
     <div
       className=""
-      onClick={() => layer.current.setHeight(0)} // Temp solution for things behind nav layer unclickable
+      onClick={() => {
+        // return layer.current.setHeight(0)
+        setShowMenu(!showMenu)
+      }} // Temp solution for things behind nav layer unclickable
     >
-      <Parallax ref={parallax} pages={4} className="bg-slate-500">
-        {/* layout */}
-
-        <ParallaxLayer factor={4} className="">
-          <div className=""></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} className="">
-          <div className="flex h-full w-full flex-col items-center justify-center">
-            <p className="text-clip text-4xl">
-              is the more info and stuff here
-            </p>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2} className=""></ParallaxLayer>
-        {/* content */}
-        <ParallaxLayer offset={0.5} speed={1} className="">
-          {" "}
-          <div className="px-2">
-            <h1 className="text-end text-6xl">Pakkerman</h1>
-            <h2 className="text-end text-4xl">Web Developer</h2>
-          </div>
-        </ParallaxLayer>
-
-        {/* nav */}
-        <ParallaxLayer
-          ref={layer}
-          offset={0.8}
-          sticky={{ start: 0.8, end: 3 }}
-          speed={1}
-          className=""
+      <nav className="">
+        <animated.div
+          style={{
+            transform: menuAnimation.x.to(
+              (value) => `translate3d(0,-${value}%,0)`
+            ),
+          }}
+          className="bg-fire fixed z-10 flex h-[30%] w-full flex-col items-center justify-evenly space-y-4"
         >
-          <div className="flex h-16 w-full items-center justify-center space-x-3">
-            <div
-              onClick={() => parallax.current.scrollTo(0)}
-              className="cursor-pointer"
-            >
-              Pakkerman
-            </div>
-            <div
-              onClick={() => parallax.current.scrollTo(1)}
-              className="cursor-pointer"
-            >
-              About
-            </div>
-            <div
-              onClick={() => parallax.current.scrollTo(2)}
-              className="cursor-pointer"
-            >
-              Projects
-            </div>
-            <div
-              onClick={() => parallax.current.scrollTo(3)}
-              className="cursor-pointer"
-            >
-              Certificates
-            </div>
+          <a className="text-2xl" href="#">
+            Home
+          </a>
+          <a className="text-2xl" href="#">
+            About
+          </a>
+          <a className="pointer-events-none text-2xl" href="#">
+            Project
+          </a>
+          <a className="text-2xl" href="#">
+            Contact
+          </a>
+        </animated.div>
+        <animated.div
+          style={{
+            transform: menuAnimation.x.to(
+              (value) => `translate3d(0,${value}%,0)`
+            ),
+          }}
+          className="preview bg-fire fixed bottom-0 z-10 h-[70%] w-full"
+        ></animated.div>
+      </nav>
+      <Parallax
+        ref={parallax}
+        pages={3}
+        // config={{ easing: easings.easeInOutExpo, duration: 0 }}
+        className="bg-fire"
+      >
+        <ParallaxLayer factor={1} offset={0} speed={-0.2} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/background.png"
+              alt=""
+              className="h-[100svh] object-cover"
+            />
           </div>
         </ParallaxLayer>
-        <ParallaxLayer
-          ref={layer}
-          offset={2}
-          sticky={{ start: 2, end: 2.5 }}
-          speed={1}
-          className=""
-        >
-          <div className="flex h-32 w-full items-center justify-center space-x-3">
-            <div
-              onClick={() => parallax.current.scrollTo(0)}
-              className="cursor-pointer"
-            >
-              Birdapp
-            </div>
-            <div
-              onClick={() => parallax.current.scrollTo(1)}
-              className="cursor-pointer"
-            >
-              Hangman
-            </div>
-            <div
-              onClick={() => parallax.current.scrollTo(2)}
-              className="cursor-pointer"
-            >
-              Beatramid
-            </div>
+        <ParallaxLayer factor={1} offset={0} speed={0} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/mountains.png"
+              alt=""
+              className="z-20 h-[100svh] object-cover"
+            />
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={1.5} className="">
-          <ProjectView />
+        <ParallaxLayer factor={1} offset={0} speed={0.2} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/jungle1.png"
+              alt=""
+              className="z-20 h-[100svh] object-cover"
+            />
+          </div>
         </ParallaxLayer>
+        <ParallaxLayer factor={1} offset={0} speed={0.4} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/jungle2.png"
+              alt=""
+              className="z-20 h-[100svh] object-cover"
+            />
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer factor={1} offset={0} speed={0.6} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/jungle3.png"
+              alt=""
+              className="z-20 h-[100svh] object-cover"
+            />
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer factor={1} offset={0} speed={0.8} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/jungle4.png"
+              alt=""
+              className="z-20 h-[100svh]  object-cover"
+            />
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer factor={1} offset={0} speed={1} className="">
+          <div className="">
+            <animated.img
+              style={fade}
+              src="./firewatch/man_on_mountain.png"
+              alt=""
+              className="z-20 h-[100svh] object-cover"
+            />
+          </div>
 
-        <ParallaxLayer offset={3} speed={1} className="">
-          {" "}
-          <div className="px-2">
-            <h1 className="text-end text-6xl">Certificates</h1>
-          </div>
+          <ParallaxLayer
+            factor={4}
+            offset={1}
+            className="bg-fire "
+          ></ParallaxLayer>
         </ParallaxLayer>
-        {/* page elements */}
-        {/* <ParallaxLayer
-          factor={0.05}
-          offset={1}
-          className="bg-slate-800 bg-opacity-40 backdrop-blur-md"
-          speed={0}
-        ></ParallaxLayer>
-        <ParallaxLayer></ParallaxLayer> */}
       </Parallax>
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
-import { type Config } from "tailwindcss";
+import { type Config } from "tailwindcss"
+const colors = require("tailwindcss/colors")
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-orange": "#d25617",
+        "custom-beige": "#d5c7b6",
+        "custom-beige-gray": "#b69e90",
+        "custom-gray-dark": "#2b2b2b",
+        "custom-gray-light": "#483d37",
+        "custom-green-gray": "#b69e90",
+        "custom-brown-gray": "#483d37",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
